a0f3de87b66d1600c4112c0d69e2bc186a5ab373
Angel Vargas <aaawwwzzzeee123@gmail.com>
2024-10-20 19:59:10 -0700

    Matrix multiplication fixed bug

--- Array2D.c
+++ Array2D.c
@@ -8,8 +8,15 @@ int main() {
 	int **arr = (int**)malloc(n * sizeof(int*));
 
 	// (3) Add your code to complete allocating and initializing the 2-D array here. The content should be all 0.
+	for(int i = 0; i < n; i++){
+		*(arr + i) = (int*)malloc(n * sizeof(int));
+	}
 
-
+	for(int i = 0; i < n; i++){ // Set every value of the sum matrix to zero
+		for(int j = 0; j < n; j++){
+			*(*(arr + i) + j) = 0;
+		}
+	}
 
     // This will print out your array
 	printArray(arr, n);
@@ -25,5 +32,11 @@ int main() {
 
 void printArray(int ** array, int size) {
     // (5) Implement your printArr here:
+	for(int i = 0; i < size; i++){
+		for(int j = 0; j < size; j++){
+			printf("%d ", *(*(array + i)+ j));
+		}
+		printf("\n");
+	}
 
 }
--- MatrixMult.c
+++ MatrixMult.c
@@ -6,23 +6,23 @@ int** matMult(int **a, int **b, int size) {
 	// You will need to create a new matrix to store the product.
 	int **matSum = (int**)malloc(size * sizeof(int*)); // initialize a temp sum Matrix
 	for(int i = 0; i < size; i++){
-		*(matSum + i) = (int*)malloc(size * sizeof(int*));
+		*(matSum + i) = (int*)malloc(size * sizeof(int));
 	}
 
-	for(int i = 0; i < size; i++){
+	for(int i = 0; i < size; i++){ // Set every value of the sum matrix to zero
 		for(int j = 0; j < size; j++){
 			*(*(matSum + i) + j) = 0;
 		}
 	}
 	
-	for(int i = 0; i < size; i++){
+	for(int i = 0; i < size; i++){ 
 		for(int j = 0; j < size; j++){
-			for(int k = 0; k < size; k++){
+			for(int k = 0; k < size; k++){ // Iterates over a[i][k] from row i and b[k][j] from column j
 				*(*(matSum + i)+ j) += *(*(a + i)+ k) * *(*(b + k)+ j);
 			}
 		}
 	}
-	return matSum;
+	return matSum; //return the sum
 }
 
 void printArray(int **arr, int n) {
